4e6b45e659707657752a9111e86312300fa81afa
ananth243 <raghavananth02@gmail.com>
2022-08-21 14:46:06 +0530

    Reconfigured database connection format

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -3,14 +3,7 @@ const { Sequelize } = require("sequelize");
 
 config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports.dbConnect = async () => {
   try {
